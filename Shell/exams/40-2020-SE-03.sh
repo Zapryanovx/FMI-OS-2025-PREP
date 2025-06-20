c7e395d81bea98c5b3bc822d06a67415a4e3b37e
Ilian Zapryanov <68477629+Zapryanovx@users.noreply.github.com>
2025-06-20 12:53:58 +0300

    Update 40-2020-SE-03.sh

--- Shell/exams/40-2020-SE-03.sh
+++ Shell/exams/40-2020-SE-03.sh
@@ -1,38 +1,32 @@
 #!/bin/bash
 
-if [[ $# -ne 2 ]]; then
+if [[ "${#}" -ne 2 ]]; then
     echo "2 args expected"
     exit 1
 fi
 
-if [[ ! -d $1 ]]; then
-    echo "first arg is not a dir"
+if [[ ! -d "${1}" ]]; then
+    echo "1st arg must be a dir"
     exit 1
 fi
 
-if [[ ! -d $2 ]]; then
-    echo "second arg is not a dir"
+if [[ ! -d "${2}" ]]; then
+    echo "2nd arg must be a dir"
     exit 1
 fi
 
 packageName=$(basename "${2}")
-version=$(cat "${2}/version" | head -n 1)
+packageVersion=$(cat "${2}/version" | head -n 1)
+	
+tar -caf temp.tar.xz "${2}/tree"
+checkSum=$(sha256sum temp.tar.xz | cut -d ' ' -f1)
+mv temp.tar.xz "${1}/packages/${checkSum}.tar.xz"
 
-if grep -E -q "^${packageName}-${version}$" "${1}/db"; then
-    checkSum=$(cat "${1}/db" | grep -E "^${packageName}-${version}" | cut -d ' ' -f2)
-    rm "${1}/packages/${checkSum}.tar.xz"
-    tar -caf temp.tar.xz "${2}/tree"
-    newCheckSum=$(sha256sum temp.tar.xz | cut -d ' ' -f1)
-    mv temp.tar.xz "${1}/packages/${newCheckSum}.tar.xz"
-    sed -E "s/^${packageName}-${version}.*/${packageName}-${version} ${newCheckSum}" "${1}/db"
+if ! grep -q -E "^${packageName}-${packageVersion}.*$" "${1}/db"; then
+	echo "${packageName}-${packageVersion} ${checkSum}" >> "${1}/db"
 else
-    tar -caf temp.tar.xz "${2}/tree"
-    checkSum=$(sha256sum temp.tar.xz | cut -d ' ' -f1)
-    mv temp.tar.xz "${1}/packages/${checkSum}.tar.xz"
-    echo "${packageName}-${version} ${checkSum}" >> "${1}/db"
-    
-    tempFile=$(mktemp)
-    cat "${1}/db" > "${tempFile}"
-    cat "${tempFile}" | sort > "${1}/db"
-    rm "${tempFile}"
+	old=$(grep -E "^${packageName}-${packageVersion}.*$" "${1}/db" | rev | cut -d ' ' -f1 | rev)
+	rm "${1}/packages/${old}.tar.xz"
+	sed -i -E "s/^${packageName}-${packageVersion}.*$/${packageName}-${packageVersion} ${checkSum}/" "${1}/db"
 fi
+
